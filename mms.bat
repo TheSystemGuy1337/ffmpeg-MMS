e5583967fdb6fa7342735692baa3a070c8e82304
TheSystemGuy1337 <61162037+TheSystemGuy1337@users.noreply.github.com>
2024-11-28 12:03:57 -0600

    Add small update to improve accuracy based on a newer 3GP file I obtained from my iPhone.

--- mms.bat
+++ mms.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p file=Select a video file to convert: 
-ffmpeg -i %file% -b:a 12.20k -acodec libopencore_amrnb -ar 8000 -ac 1 -b:v 126k -vf scale=176x144 -pix_fmt yuv420p -r 10 %file%.3gp
+ffmpeg -i %file% -b:a 12.20k -acodec libopencore_amrnb -ar 8000 -ac 1 -b:v 103k -vf scale=176x144,setsar=12/11,setdar=4/3 -pix_fmt yuv420p -r 10 %file%.3gp
 echo Your output file is: %file%.3gp
